77b3bce312503a23e74301d5c4ea278eef33f9a4
iohongwal <iohongwal@gmail.com>
2026-07-02 00:41:10 -0400

    Add: course-schedule-iv - submission-2

---
+++ Data Structures & Algorithms/course-schedule-iv/submission-2.py
@@ -0,0 +1,44 @@
+class Solution:
+    def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
+        precourse_hash = {i:[] for i in range(numCourses)}
+
+        for precourse, course in prerequisites:
+            precourse_hash[course].append(precourse)
+        
+        
+        answer = []
+
+        def dfs(query_course, course):
+            if course in visited:
+                return False
+
+            #loop detected
+            if course in visiting:
+                return False
+
+            if course == query_course:
+                return True
+
+            visiting.add(course)
+
+            for precourse in precourse_hash[course]:
+                if dfs(query_course, precourse):
+                    return True
+            
+            visited.add(course)
+            visiting.remove(course)
+            return False
+
+        answer = []
+        for precourse, course in queries:
+            #Record the visited node
+            visited = set()
+            #Record the visting node of current dfs loop
+            #Aimed to detect looped graph
+            visiting = set()
+            answer.append(dfs(precourse, course))
+        
+        return answer
+
+
+
