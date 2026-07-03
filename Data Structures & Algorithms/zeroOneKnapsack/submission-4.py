47bc5ea6b9dbe573d5005876749d121bdcafd1e7
iohongwal <iohongwal@gmail.com>
2026-07-02 22:55:20 -0400

    Add: zeroOneKnapsack - submission-4

---
+++ Data Structures & Algorithms/zeroOneKnapsack/submission-4.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maximumProfit(self, profit: List[int], weight: List[int], capacity: int) -> int:
+        n, m = len(profit), capacity
+        dp = [0] * (m + 1)
+
+        for cap in range(m + 1):
+            if cap >= weight[0]:
+                dp[cap] = profit[0]
+        
+        for i in range(1, n):
+            curRow = [0] * (m + 1)
+            for cap in range(1, m + 1):
+                skip = dp[cap]
+                include = 0
+                if cap - weight[i] >= 0:
+                    include = profit[i] + dp[cap - weight[i]]
+                curRow[cap] = max(skip, include)
+            dp = curRow
+
+        return dp[m]
+
