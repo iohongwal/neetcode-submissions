67c8e11c0e575be9e9b5f751e42b143ed74fba2c
iohongwal <iohongwal@gmail.com>
2026-07-04 00:41:43 -0400

    Add: coin-change - submission-8

---
+++ Data Structures & Algorithms/coin-change/submission-8.py
@@ -0,0 +1,51 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+                
+        if amount < 1:
+            return 0
+
+        n = len(coins)
+
+        #dp[0...amount]
+        dp = [float('inf')] * (amount + 1)
+
+        dp[0] = 0
+
+        for change in range(coins[0], amount + 1):
+            dp[change] = 1 + dp[change - coins[0]]
+
+        for i in range(1, n):
+            for change in range(1 , amount + 1):
+                if change >= coins[i]:
+                    dp[change] = min(
+                        dp[change], 
+                        1 + dp[change - coins[i]]
+                        )
+        
+        return dp[-1] if dp[-1] != float('inf') else -1
+
+        # # 2D DP
+        # if amount < 1:
+        #     return 0
+
+        # n = len(coins)
+
+        # #dp[0...n-1]][0...amount]
+        # dp = [[float('inf')] * (amount + 1) for _ in range(n)]
+
+        # for i in range(n):
+        #     dp[i][0] = 0
+
+        # for change in range(coins[0], amount + 1):
+        #     dp[0][change] = 1 + dp[0][change - coins[0]]
+
+        # for i in range(1, n):
+        #     for change in range(1 , amount + 1):
+        #         dp[i][change] = dp[i - 1][change]
+        #         if change >= coins[i]:
+        #             dp[i][change] = min(
+        #                 dp[i][change], 
+        #                 1 + dp[i][change - coins[i]]
+        #                 )
+        
+        # return dp[-1][-1] if dp[-1][-1] != float('inf') else -1
