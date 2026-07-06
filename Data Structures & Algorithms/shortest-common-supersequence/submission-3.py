ca016e3d6e1b33c4b6cf65900f063913c34dc7ee
iohongwal <iohongwal@gmail.com>
2026-07-06 00:47:14 -0400

    Add: shortest-common-supersequence - submission-3

---
+++ Data Structures & Algorithms/shortest-common-supersequence/submission-3.py
@@ -0,0 +1,90 @@
+class Solution:
+    def shortestCommonSupersequence(self, str1: str, str2: str) -> str:
+        #Best Solution
+        n, m = len(str1), len(str2)
+
+        dp = [[0] * (m + 1) for _ in range(n + 1)]
+
+        for i in range(1, n + 1):
+            for j in range(1, m + 1):
+                if str1[i - 1] == str2[j - 1]:
+                    dp[i][j] = 1 + dp[i - 1][j - 1]
+                else:
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+        
+        i, j = n, m
+        res = []
+
+        while i > 0 and j > 0:
+            if str1[i - 1] == str2[j - 1]:
+                res.append(str1[i - 1])
+                i -= 1
+                j -= 1
+            elif dp[i - 1][j] > dp[i][j - 1]:
+                res.append(str1[i - 1])
+                i -= 1
+            else:
+                res.append(str2[j - 1])
+                j -= 1
+        
+        while i > 0:
+            res.append(str1[i - 1])
+            i -= 1
+        
+        while j > 0:
+            res.append(str2[j - 1])
+            j -= 1
+
+        return "".join(reversed(res))
+
+
+        #DP
+        # n, m = len(str1), len(str2)
+
+        # dp = [[""] * (m + 1) for _ in range(n + 1)]
+
+        # for i in range(n, -1, -1):
+        #     for j in range(m, -1, -1):
+        #         if i >= n:
+        #             dp[i][j] = str2[j:]
+        #         elif j >= m:
+        #             dp[i][j] = str1[i:]
+        #         elif str1[i] == str2[j]:
+        #             dp[i][j] = str1[i] + dp[i + 1][j + 1]
+        #         else:
+        #             res1 = str1[i] + dp[i + 1][j]
+        #             res2 = str2[j] + dp[i][j + 1]
+        #             dp[i][j] = res1 if len(res1) < len(res2) else res2
+
+        # return dp[0][0]
+
+        # #memorization
+        # cache = {}
+
+        # def bracktrack(i, j):
+        #     if i == len(str1):
+        #         return str2[j:]
+            
+        #     if j == len(str2):
+        #         return str1[i:]
+            
+        #     if (i, j) in cache:
+        #         return cache[(i, j)]
+
+        #     if str1[i] == str2[j]:
+        #         return str1[i] + bracktrack(i + 1, j + 1)
+
+            
+        #     res1 = str1[i] + bracktrack(i + 1, j)
+        #     res2 = str2[j] + bracktrack(i, j + 1)
+
+        #     if len(res1) < len(res2):
+        #         cache[(i, j)] = res1
+        #         return res1
+
+        #     cache[(i, j)] =  res2
+
+        #     return res2
+
+        # return bracktrack(0, 0)
+                
