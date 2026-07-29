a3f7b7a3f2b0abb0aadf10b5b2cf8fc65d8c33cf
iohongwal <iohongwal@gmail.com>
2026-07-28 23:04:56 -0400

    Add: time-based-key-value-store - submission-4

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-4.py
@@ -0,0 +1,27 @@
+class TimeMap:
+
+    def __init__(self):
+        self.arr = {}
+        self.key = {}
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.arr[(key, timestamp)] = value
+        if key not in self.key:
+            self.key[key] = []
+        self.key[key].append(timestamp)
+
+    def get(self, key: str, timestamp: int) -> str:
+        if (key, timestamp) in self.arr:
+            return self.arr[(key, timestamp)]
+        keyArr = self.key.get(key, [])
+        l, r = 0, len(keyArr) - 1
+        res = ""
+        while l <= r:
+            m = (l + r) //2
+            if keyArr[m] <= timestamp:
+                res = self.arr[(key, keyArr[m])]
+                l = m + 1
+            else:
+                r = m - 1
+
+        return res
+        
