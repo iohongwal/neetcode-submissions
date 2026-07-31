dd7d05d689a1e07be1e67ea8ad1ee61255e60925
iohongwal <iohongwal@gmail.com>
2026-07-30 23:52:30 -0400

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.py
@@ -0,0 +1,11 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = collections.defaultdict(list)
+
+        for s in strs:
+            letterKey = [0] * 26
+            for c in s:
+                letterKey[ord(c) - ord('a')] += 1
+            res[tuple(letterKey)].append(s)
+        
+        return list(res.values())
