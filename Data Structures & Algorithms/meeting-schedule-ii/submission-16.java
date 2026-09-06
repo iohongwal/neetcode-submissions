7c02ad67de7b051494176d5419538c4f4f804d49
iohongwal <iohongwal@gmail.com>
2026-09-06 00:17:04 -0400

    Add: meeting-schedule-ii - submission-16

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-16.java
@@ -0,0 +1,41 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+    public int minMeetingRooms(List<Interval> intervals) {
+        if (intervals.size() < 1) return intervals.size();
+        int[] start = new int[intervals.size()];
+        int[] end = new int[intervals.size()];
+        for (int i = 0; i < intervals.size(); i++){
+            start[i] = intervals.get(i).start;
+            end[i] = intervals.get(i).end;
+        }
+        Arrays.sort(start);
+        Arrays.sort(end);
+        int s = 0;
+        int e = 0;
+        int count = 0;
+        int maxRooms = 0;
+        while (s < start.length){
+            if (start[s] < end[e]){
+                count++;
+                maxRooms = Math.max(maxRooms, count);
+                s++;
+            }
+            else{
+                count--;
+                e++;
+            }
+        } 
+
+        return maxRooms;
+    }
+}
