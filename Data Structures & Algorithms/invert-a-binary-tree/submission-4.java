e1341c3bf17c77963ed6dfae3263714999f78a03
iohongwal <iohongwal@gmail.com>
2026-09-08 11:08:24 -0400

    Add: invert-a-binary-tree - submission-4

---
+++ Data Structures & Algorithms/invert-a-binary-tree/submission-4.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+
+        TreeNode left_tree = invertTree(root.left);
+        TreeNode right_tree = invertTree(root.right);
+
+        root.left = right_tree;
+        root.right = left_tree;
+
+        return root; 
+    }
+}
